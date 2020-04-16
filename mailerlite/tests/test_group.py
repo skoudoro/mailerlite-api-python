afc709d4976c6bf8f3ba6c9297a67eee161089ea
Serge Koudoro <skab12@gmail.com>
2020-04-16 03:47:46 +0200

    add more test for groups

--- mailerlite/tests/test_group.py
+++ mailerlite/tests/test_group.py
@@ -1,8 +1,10 @@
+"""Module to tests groups."""
 from mailerlite.group import Groups
 from mailerlite.constants import API_KEY_TEST, Group
+import pytest
 
 
-def test_groups():
+def test_groups_instance():
     headers = {
         'content-type': "application/json",
         'x-mailerlite-apikey': API_KEY_TEST
@@ -28,3 +30,33 @@ def test_groups():
     assert len(res) > 0
     for g in res:
         assert isinstance(g, Group)
+
+
+def test_groups_crud():
+    headers = {
+        'content-type': "application/json",
+        'x-mailerlite-apikey': API_KEY_TEST
+    }
+
+    groups = Groups(headers)
+
+    expected_group_name = "TEST_K_GROUP"
+    expected_group_name_2 = "TEST_GROUP_KKK"
+    e_res = groups.create(expected_group_name)
+    assert e_res.name == expected_group_name
+
+    res = groups.get(e_res.id)
+    assert res.name == expected_group_name
+
+    groups.update(e_res.id, expected_group_name_2)
+    res = groups.get(e_res.id)
+    assert res.name == expected_group_name_2
+
+    assert groups.delete(e_res.id)
+
+    with pytest.raises(OSError):
+        groups.get(e_res.id)
+
+
+def test_groups_subscriber():
+    pass
