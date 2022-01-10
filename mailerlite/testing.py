5873f8da68155af47f0dbed86471346b7b34e6cf
Serge Koudoro <skab12@gmail.com>
2022-01-10 04:57:40 +0100

    update decorator

--- mailerlite/testing.py
+++ mailerlite/testing.py
@@ -1,13 +1,16 @@
 """Utility function for unit tests."""
+from functools import wraps
 import pytest
 
 
 def succeed_or_skip_sensitive_tests(func):
     """Small decorator to skip some sensitive function."""
-    def wrapper_func():
+    @wraps(func)
+    def wrapper_func(*args, **kwargs):
         try:
-            func()
-        except Exception:
+            func(*args, **kwargs)
+        except Exception as e:
+            print(str(e))
             pytest.skip("Test Failed due to an object deleted "
                         "by another matrix.")
 
